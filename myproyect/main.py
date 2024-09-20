000ba1bb55f36c989e9796e9268f5d784478ad14
lmiel <lucia.mielgo@gmail.com>
2024-09-20 19:05:12 +0200

    updatee

--- myproyect/main.py
+++ myproyect/main.py
@@ -1,9 +1,18 @@
 from subclases.Coche import Coche
 from subclases.Bicicleta import Bicicleta
+from subsubclases.Motocicleta import Motocicleta
+from subsubclases.Camioneta import Camioneta
+
 
 if __name__ == "__main__":
-    coche = Coche("azul", 4, 150, 1200)
-    print(coche)
+    vehiculo1 = Coche("azul", 4, 150, 1200)
+    print(vehiculo1)
     
-    bicicleta = Bicicleta("rojo", 2, "urbana")
-    print(bicicleta)
+    vehiculo2 = Bicicleta("rojo", 2, "urbana")
+    print(vehiculo2)
+
+    vehiculo3 = Camioneta("blanco", 4, 100, 1300, 1500)
+    print(vehiculo3)
+
+    vehiculo4 = Motocicleta("gris", 2, "deportiva", 180, 900)
+    print(vehiculo4)
---
+++ myproyect/subsubclases/Camioneta.py
@@ -0,0 +1,9 @@
+from subclases.Coche import Coche
+
+class Camioneta(Coche):
+    def __init__(self, color, ruedas, velocidad, cilindrada, carga):
+        super().__init__(color, ruedas, velocidad, cilindrada)
+        self.carga = carga
+    
+    def __str__(self):
+        return super().__str__() + ", {}kg de carga".format(self.carga)
