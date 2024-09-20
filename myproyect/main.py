3eaa99c3d8050fc4ecc18f5fc725fc0b7ad67917
lmiel <lucia.mielgo@gmail.com>
2024-09-20 19:31:01 +0200

    Add vehiculos to main.py

--- myproyect/main.py
+++ myproyect/main.py
@@ -5,14 +5,20 @@ from subsubclases.Camioneta import Camioneta
 
 
 if __name__ == "__main__":
+    vehiculos = []
+
     vehiculo1 = Coche("azul", 4, 150, 1200)
+    vehiculos.append(vehiculo1)
     print(vehiculo1)
     
     vehiculo2 = Bicicleta("rojo", 2, "urbana")
+    vehiculos.append(vehiculo2)
     print(vehiculo2)
 
     vehiculo3 = Camioneta("blanco", 4, 100, 1300, 1500)
+    vehiculos.append(vehiculo3)
     print(vehiculo3)
 
     vehiculo4 = Motocicleta("gris", 2, "deportiva", 180, 900)
+    vehiculos.append(vehiculo4)
     print(vehiculo4)
